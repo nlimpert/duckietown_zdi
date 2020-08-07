9c0252558904316c0a10817d254339033446d571
Nicolas Limpert <limpert@fh-aachen.de>
2020-08-07 10:26:45 +0200

    update teleop node

--- duckiebot_teleop_nlimpert/src/duckiebot_teleop.py
+++ duckiebot_teleop_nlimpert/src/duckiebot_teleop.py
@@ -2,12 +2,12 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from duckietown_msgs.msg import WheelsCmdStamped
+from duckietown_msgs.msg import Twist2DStamped
 
 class DuckiebotTeleop:
    def __init__(self):
      rospy.Subscriber("cmd_vel", Twist, self.callback)
-     self.pub = rospy.Publisher("/duckie1/wheels_driver_node/wheels_cmd", WheelsCmdStamped, queue_size=10)
+     self.pub = rospy.Publisher("/None/lane_controller_node/car_cmd", Twist2DStamped, queue_size=10)
 
      rospy.spin()
 
@@ -15,10 +15,10 @@ class DuckiebotTeleop:
       rospy.loginfo("V: %f", data.linear.x)
       rospy.loginfo("Omega: %f", data.angular.z)
 
-      wheels = WheelsCmdStamped()
-      wheels.vel_left  = data.linear.x / 10
-      wheels.vel_right = data.linear.x / 10
-      self.pub.publish(wheels)
+      twist = Twist2DStamped()
+      twist.v     = data.linear.x  / 5
+      twist.omega = data.angular.z * 1.5
+      self.pub.publish(twist)
 
 if __name__ == '__main__':
    rospy.init_node("duckiebot_teleop", anonymous=True)
